c0dc7ee1c496b63eb85b56a547425c4f1951ca4d
Tran Van Thuc <tranvanthucduytan@gmail.com>
2017-07-04 20:17:13 +0700

    Finished Homepage

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import stylesheet from './Homepage.css';
+import { Row } from 'reactstrap';
+
 
 // <div className='card'>
 //         <div className="card-content">
@@ -32,7 +35,41 @@ import { Link } from 'react-router-dom';
 class HomePage extends Component {
   render() {
     return (
-      <div> lala</div>
+      <div>
+        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+
+        <div className="main_content d-flex flex-column ">
+          <div className="title"><h3>Welcome to Sunway ATM</h3></div>
+          <div className="d-flex flex-column">
+            <div className="d-flex flex-row">
+              <div className="item item_1">
+                <Link to='/check-account' className='link'>
+                 Check account
+               </Link>
+              </div>
+              <div className="item item_2">
+                <Link to='/withdraw' className='link'>
+                 Withdraw
+               </Link>
+              </div>
+
+            </div>
+            <div className="d-flex flex-row">
+              <div className="item item_3">
+                <Link to='/transfer' className='link'>
+                  Transfer
+                </Link>
+              </div>
+              <div className="item item_4">
+                <Link to='/transfer' className='link'>
+                 Deposit
+                </Link>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
