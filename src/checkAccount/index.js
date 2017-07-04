8f5342c322502445b318c3603d9cb5e5901c9174
thanhvinhlu <luthanhvinhdtu@gmail.com>
2017-07-04 20:54:07 +0700

    fix check account UI

--- src/checkAccount/index.js
+++ src/checkAccount/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import api from '../api';
+import  stylesheet from './CheckAccount.css';
+import { Row, Col,Button } from 'reactstrap';
+
+
 
 class CheckAccount extends Component {
   static propTypes = {
@@ -15,6 +19,7 @@ class CheckAccount extends Component {
     }
 
     this.handleGoBack = this.handleGoBack.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
   }
   async componentDidMount() {
     const res = await api.get('/account');
@@ -25,13 +30,23 @@ class CheckAccount extends Component {
   handleGoBack() {
     this.props.history.goBack();
   }
+  handleCancel(){
+    alert('Thank you for using our service!');
+  }
   render() {
     return (
       <div className="card">
+        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
         <div className="card-content">
-          <h1 className='title is-3' >Your account ballance is</h1>
-          <h1 className='title is-3'>{this.state.money}$</h1>
-          <button onClick={this.handleGoBack} className="button is-primary">Back</button>
+          <p className='title'> Your account ballance is <br/> {this.state.money}$ </p>
+          <br/>
+          <p style={{textAlign: "center"}}>------------------</p>
+          <div className='title'> Do you want to make <br/> another transaction? <br/>
+            <div className="d-flex justify-content-around set_button">
+                <div><Button onClick={this.handleGoBack} className="button" outline color="success">Yes</Button></div>
+                <div><Button onClick={this.handleCancel} className="button" outline color="danger">No</Button></div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <BrowserRouter>
